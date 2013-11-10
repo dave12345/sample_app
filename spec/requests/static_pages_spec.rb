ee91f448011b3fb28fce2cd2ac3a1747bdf990b5
David <dothedu22@yahoo.com>
2013-11-10 14:41:13 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,58 @@
 require 'spec_helper'
-  describe "Static pages" do
-    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-    describe "Home page" do
-      it "should have the h1 'Sample App'" do
-        visit '/static_pages/home'
-        page.should have_selector('h1', :text => 'Sample App')
-      end
+describe "Static pages" do
 
-      it "should have the title 'Home'" do
-        visit '/static_pages/home'
-        page.should have_selector('title', :text => "#{base_title} | Home")
-      end
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
   end
-    
-    describe "Help page" do
-        
-        it "should have the h1 'Help'" do
-            visit '/static_pages/help'
-            page.should have_selector('h1', :text => 'Help')
-        end
-        
-        it "should have the title 'Help'" do
-            visit '/static_pages/help'
-            page.should have_selector('title', :text => "#{base_title} | Help")
-        end
-    end
-    
-    describe "About page" do
-        
-        it "should have the content 'About Us'" do
-            visit '/static_pages/about'
-            page.should have_selector('h1', :text => 'About Us')
-        end
-        
-        it "should have the title 'About Us'" do
-            visit '/static_pages/about'
-            page.should have_selector('title', :text => "#{base_title} | About Us")
-        end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
 
-   describe "Contact page" do
-        
-        it "should have the content 'Contact'" do
-            visit '/static_pages/contact'
-            page.should have_selector('h1', :text => 'Contact')
-        end
-        
-        it "should have the title 'Contact'" do
-            visit '/static_pages/contact'
-            page.should have_selector('title', :text => "#{base_title} | Contact")
-        end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
     end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
 end
